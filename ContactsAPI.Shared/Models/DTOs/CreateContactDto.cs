99bc714adf5c9f54b670df558c186b66d09d0c3a
Olorunfemi Davis <fzanyajibs@outlook.com>
2023-02-26 19:25:22 +0100

    Add README.md Add comments

--- ContactsAPI.Shared/Models/DTOs/CreateContactDto.cs
+++ ContactsAPI.Shared/Models/DTOs/CreateContactDto.cs
@@ -2,17 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ContactsAPI.Shared.Models.DTOs;
 
+/// <summary>
+/// Model for Contact Creation
+/// </summary>
 public class CreateContactDto
 {
     /// <summary>
     /// Contact Name
     /// </summary>
+    /// <example>Olorufemi</example>
     [Required]
     public string Name { get; set; }
     
     /// <summary>
     /// Phone number in local format
     /// </summary>
+    /// <example>07001234455</example>
     [Required]
     public string Phone { get; set; }
 }
